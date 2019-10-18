12dd5b0e0c409361fe439d5918a83bef4241c447
lindskelsey <lindsaykelsey@gmail.com>
2019-10-17 19:26:21 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+#Workforce Management App
---
+++ src/test/java/org/launchcode/workforce/WorkforceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.workforce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkforceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
