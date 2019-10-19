5a4062f94df4dfa7eafd74eaee8e862056df342c
tspencer0917 <tns3327@truman.edu>
2019-10-19 18:56:12 -0500

    implemented Job Location API endpoints (#1)

---
+++ src/main/java/org/launchcode/workforce/model/JobLocation.java
@@ -0,0 +1,26 @@
+package org.launchcode.workforce.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Table
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class JobLocation {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "employer")
+    private String employer;
+
+    @Column(name = "address")
+    private String address;
+}
