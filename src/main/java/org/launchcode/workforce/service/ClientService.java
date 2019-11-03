b236f93d8f0e7c3d56ef806c14ca469c7c1f2aa4
angiedawn <51460230+angiedawn@users.noreply.github.com>
2019-11-03 11:16:49 -0600

    Changed ClientName to Client (#9)

---
+++ src/main/java/org/launchcode/workforce/service/ClientService.java
@@ -0,0 +1,43 @@
+package org.launchcode.workforce.service;
+
+import lombok.Getter;
+import org.launchcode.workforce.model.Client;
+import org.launchcode.workforce.repository.ClientRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ClientService {
+
+    private ClientRepository clientRepository;
+
+    public ClientService(ClientRepository clientRepository){
+        this.clientRepository = clientRepository;
+    }
+    public List<Client> getAll(){
+        return clientRepository.findAll();
+    }
+
+    public Optional<Client> get(Long id) {
+        return clientRepository.findById(id);
+    }
+    public Client addClient(Client client){
+        return clientRepository.save(client);
+    }
+    public Optional<Client> update(Client client) {
+        if(clientRepository.existsById(client.getId())){
+            clientRepository.save(client);
+            return Optional.of(client);
+        }
+        return Optional.empty();
+    }
+    public boolean delete(Long id) {
+        if (clientRepository.existsById(id)) {
+            clientRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+}
