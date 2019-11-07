2e9fdd7ffd022a3b4e4cbb6162b9529b9fbb48e4
Kevin Platt <onlycraft@gmail.com>
2019-11-07 12:20:40 -0600

    TimeStamp with functional improvements suggested

--- src/main/java/org/launchcode/workforce/controller/TimeStampController.java
+++ src/main/java/org/launchcode/workforce/controller/TimeStampController.java
@@ -5,9 +5,8 @@ import org.launchcode.workforce.service.TimeStampService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
-import java.util.Optional;
+
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/timeClock")
@@ -19,12 +18,11 @@ public class TimeStampController {
         this.timeStampService = timeStampService;
     }
 
-    //how will you label between clock in and clock out
     @PostMapping("/clock")
     @ResponseStatus(HttpStatus.OK)
-    TimeStamp add(@RequestBody TimeStamp timeStamp) {
-        return timeStampService.add(timeStamp);
-    }
+    TimeStamp add(@RequestBody Long clientId) {
+        return timeStampService.add(clientId);
+        }
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
@@ -32,16 +30,17 @@ public class TimeStampController {
         return timeStampService.getAll();
     }
 
-
     //adjust to only provide within a certain date range
     @GetMapping("/{id}")
-    ResponseEntity<TimeStamp> get(@PathVariable Long clientId) {
-        Optional<TimeStamp> timeStamp = timeStampService.get(clientId);
-        if (timeStamp.isPresent()) {
-            return ResponseEntity.ok().body(timeStamp.get());
+    ResponseEntity<TimeStamp> get(@PathVariable Long id) {
+        List<TimeStamp> timeStamp = timeStampService.getByClient(id);
+        if (timeStamp.indexOf(0) == -1) {
+            //modify to allow for a first clock in
+            return ResponseEntity.notFound().build();
+
         }
         else {
-            return ResponseEntity.notFound().build();
+            return ResponseEntity.ok().body(timeStamp.get(Math.toIntExact(id)));
         }
 
     }
