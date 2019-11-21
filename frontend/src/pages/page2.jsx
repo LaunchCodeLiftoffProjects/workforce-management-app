cf8399f39b5f76d76b8d7f585982858fdc2a4677
Linds Kelsey <lindsaykelsey@gmail.com>
2019-11-20 20:25:44 -0600

    Palette change + more verbose code (#12)
    * add ability to change palette
    
    * update routes with new component names

--- frontend/src/pages/page2.jsx
+++ frontend/src/pages/page2.jsx
@@ -1,5 +1,13 @@
-import React from 'react'
+import React from "react";
 
-export default () => {
-    return <di>Page 2</di>
+export default class PageTwo extends React.Component {
+  //Write HTML inside render function
+  render() {
+    return (
+      <div>
+        <h1>Page 2</h1>
+        <p>Welcome to the second page!</p>
+      </div>
+    );
+  }
 }
--- frontend/src/pages/page5.jsx
+++ frontend/src/pages/page5.jsx
@@ -1,5 +1,13 @@
-import React from 'react'
+import React from "react";
 
-export default () => {
-    return <di>Page 5</di>
+export default class PageFive extends React.Component {
+  //Write HTML inside render function
+  render() {
+    return (
+      <div>
+        <h1>Page 5</h1>
+        <p>Welcome to the fifth page!</p>
+      </div>
+    );
+  }
 }
